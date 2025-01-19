448bfcd73ef74a39eabdb2c8466963657a120d4d
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-01-19 15:46:34 +0300

    feat: auto-load 15 cats on page

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const catContainer = document.getElementById("cat_container");
 
+const API_KEY =
+  "live_4WcBlncOaCyN9rrTlXFHhNOTTi23otwDdF56QjP7TAWgewELjY8Dw7B90P8omjLF";
+
 async function getCats() {
   const response = await fetch(
-    "https://api.thecatapi.com/v1/images/search?limit=10"
+    "https://api.thecatapi.com/v1/images/search?limit=15",
+    {
+      headers: {
+        "x-api-key": API_KEY,
+      },
+    }
   );
   const data = await response.json();
   return data;
@@ -17,8 +25,9 @@ async function loadCats() {
       const img = document.createElement("img");
       img.src = cat.url;
       img.alt = "Cute Cat";
-      img.style.width = "200px";
-      img.style.margin = "10px";
+      img.style.width = "225px";
+      img.style.height = "225px";
+      img.classList.add("cat-image");
       catContainer.appendChild(img);
     });
   } else {
